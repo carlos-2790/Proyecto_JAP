202fcadcb0d2aec6c4b94c42c6e65c8bc40d86fd
carlos-2790 <68618058+carlos-2790@users.noreply.github.com>
2020-09-04 00:27:16 -0300

    entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,10 @@
 var product = {};
+var coments="";
+let arrays=[];
+let infoHtml=" ";
+
+
+
 
 function showImagesGallery(array){
 
@@ -18,6 +24,7 @@ function showImagesGallery(array){
         document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
     }
 }
+
 
 
 function showRelatedProducts(array){
@@ -66,6 +73,87 @@ document.addEventListener("DOMContentLoaded", function(e){
             showRelatedProducts(product.relatedProducts);
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
+            
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function(ex){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultado){
+        if(resultado.status === "ok"){
+            coments=resultado.data;
+            for(let i=0; i<coments.length; i++){
+           let com=coments[i];
+           
+            infoHtml +=`
+            <strong> Puntuacion:  </strong> `+com.score +`<br>
+            <strong> Descripcion: </strong>`+com.description +`<br>
+            <strong> Usuario:     </strong> ` + com.user+`<br>
+            <strong> Fecha:       </strong> `+ com.dateTime+`<br><br>
+
+               `
+               document.getElementById("contenedor-info").innerHTML=infoHtml;
+            }
+            
+        }
+        
+    })
+
+
+});
+
+var d= new Date();
+var dia=d.getDay();
+dia=dia-1;
+var mes= d.getMonth();
+mes=mes+1;
+var anio= d.getUTCFullYear();
+var hora= d.getHours();
+var min= d.getMinutes();
+var seg= d.getSeconds();
+
+ var stringFechas=` ` +dia+`-`+mes+`-`+anio +`  `+hora +`:`+min+`:`+seg+``;
+ var fechas =document.getElementById("fechaHoy");
+ fechas.innerHTML=stringFechas;
+ var puntaje= document.getElementById("puntaje");
+ var comentario= document.getElementById("comentario");
+ var usuarioC= localStorage.getItem('usuario');
+
+ 
+
+
+
+ function agregarComentario(){
+
+
+    if(puntaje.value == null ||puntaje.value == ""){
+    
+       alert("Debe ingresar un valor en Puntaje...")
+    }
+   /*else if(puntaje.value !== /^[0-9]$/){
+        alert("El valor ingresado en Puntaje debe ser numérico...")
+    }*/
+    else if( puntaje.value <1 || puntaje.value >5){
+        alert("El puntaje debe estar entre 1 y 5 ")
+    }
+    else if(comentario.value == null || comentario.value == "" ){
+        alert("Debe ingresar un comentario...")
+    }else{
+
+
+    
+    infoHtml +=`
+            <strong> Puntuacion:  </strong> `+puntaje.value +`<br>
+            <strong> Descripcion: </strong>`+comentario.value +`<br>
+            <strong> Usuario:     </strong> ` +usuarioC+`<br>
+            <strong> Fecha:       </strong> `+stringFechas+`<br><br>
+
+               `
+               document.getElementById("contenedor-info").innerHTML=infoHtml;
+               puntaje.value="";
+               comentario.value="";
+               
+            }
+
+
+ }
--- js/usuarioWeb.js
+++ js/usuarioWeb.js
@@ -1,5 +1,14 @@
+//LE PASO EL "ID" DE DROP DOWN DEL USUARIO A IMPRIMIR
 const imprimir = document.getElementById('usuario_nombre');
 
+// LE PASO EL "ID" DEL LABEL PARA IMPRIMIR EL NOMBRE DE USUARIO
+const usuComent = document.getElementById("usua");
+
+//TRAIGO EL NOMBRE DE USUARIO EN LA VARIABLE "usuario" DEL LOCALSTORAGE
 var usu =localStorage.getItem('usuario');
 
+//IMPRIMO EL NOMBRE DE USUARIO 
 imprimir.innerHTML=usu;
+
+//IMPRIMO EL USUARIO EN EL LABEL DEL COMENTARIO
+usuComent.innerHTML=`<label for="text" id="usua">`+usu+`</label>`;

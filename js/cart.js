3fea57c42fd03ae6d80d7c98b8c275c1cf126de8
carlos-2790 <68618058+carlos-2790@users.noreply.github.com>
2020-10-06 18:08:58 -0300

    entrega 5

--- js/cart.js
+++ js/cart.js
@@ -5,21 +5,124 @@ let precioUnit = 0;
 let precioUnit2 = 0;
 let num = 0;
 let num2 = 0;
+let impSubtotal = 0;
+let impSub2 = 0;
+
+let subT = 0;
+var subT2 = 0;
+var porcentajeEnvio = 0.15;
 
 
 
-//funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
-document.getElementById("numero").addEventListener("click", function () {
-    var num = document.getElementById("numero").value;
-    document.getElementById("subTotal").innerHTML ='UYU ' +num * 100;
+
+function actualizarCostos() {
+
+    let subText = document.getElementById("subtotalText");
+    let envText = document.getElementById("envioText");
+    let totalText = document.getElementById("totalCostText");
+
+    //calculo el precio unitario del pino
+    var t1 = (subT * 100);
+    //calculo el precio unitario de autos y lo paso a pesos 
+    var t2 = (subT2 * 12500 * 40);
+    var t = (t2 + t1);
+
+    let subtextHTML = t;
+    let costoEnvioHTML = (porcentajeEnvio * t);
+    let totalEnvioHTML = Math.round(t + costoEnvioHTML);
+
+    subText.innerHTML = subtextHTML;
+    envText.innerHTML = costoEnvioHTML;
+    totalText.innerHTML = totalEnvioHTML;
+
+
+    let detalleHtml = document.getElementById("detalleCompra");
+    detalleHtml.innerHTML = ` Total a pagar $ ` + totalEnvioHTML;
+
+
+}
+
+
+
+//funcion que se ejecuta cuando se hace click en el boton aceptar
+function btnAceptar() {
+
+    document.getElementById("calle").value="";
+    document.getElementById("numeroL").value="";
+    document.getElementById("esquina").value ="";
+    document.getElementById("detalleCompra").innerHTML = '';
+    document.getElementById("subtotalText").innerHTML = 0;
+    document.getElementById("envioText").innerHTML = 0;
+    document.getElementById("totalCostText").innerHTML = 0;
+    document.getElementById("numero").value = 0;
+    document.getElementById("numero2").value = 0;
+    document.getElementById("subTotal").innerHTML = 0;
+    document.getElementById("subTotal2").innerHTML = 0;
+
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+
+    //funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
+    document.getElementById("numero").addEventListener("change", function () {
+        var num = document.getElementById("numero").value;
+
+        impSubtotal = document.getElementById("subTotal");
+        impSubtotal.innerHTML = num * 100;
+        subT = 100 * num;
+        subT = this.value
+
+        actualizarCostos();
+
+    });
+
+    //funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
+    document.getElementById("numero2").addEventListener("change", function () {
+        var num2 = document.getElementById("numero2").value;
+
+        impSub2 = document.getElementById("subTotal2");
+        impSub2.innerHTML = (40 * 12500) * num2;
+        subT2 = (Math.round(12500 * 40) * impSub2);
+
+        subT2 = this.value;
+        actualizarCostos();
+
+    });
+
+
+    document.getElementById("premiumradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.15;
+        actualizarCostos()
+    });
+
+    document.getElementById("expressradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.07;
+        actualizarCostos()
+    });
+
+    document.getElementById("standardradio").addEventListener("change", function () {
+        porcentajeEnvio = 0.05;
+        actualizarCostos()
+    });
+
+
+
+
+
+
+
+
 });
-//funcion que trae el numero del input, se lo paso a una variable  e imprimo en otro id el monto.
-document.getElementById("numero2").addEventListener("click", function () {
-    var num2 = document.getElementById("numero2").value;
 
-    document.getElementById("subTotal2").innerHTML ='UYU '+(40 * 12500) * num2;
 
-});
+
+
+
+
 
 
 
